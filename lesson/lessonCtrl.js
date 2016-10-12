74678ea9ee5764a5ab94dda590c2f25f1d4291d6
Eric Anderson <splanderson@gmail.com>
2016-10-11 19:46:39 -0600

    created basic app

---
+++ lesson/lessonCtrl.js
@@ -0,0 +1,5 @@
+app
+  .controller('lessonController', function($scope){
+    $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
+    $scope.test = 'Two-way data binding!';
+  });
---
+++ lesson/lessonHider.js
@@ -0,0 +1,24 @@
+app
+  .directive('lessonHider', function(){
+    return {
+      templateUrl: 'lesson/lessonHider.html',
+      restrict: 'E',
+      scope: {
+        lesson: '='
+      },
+      controller: function($scope, lessonService){
+          $scope.getSchedule = lessonService.getSchedule();
+      },
+      link: function(scope, element, attributes) {
+        scope.getSchedule.then(function(response){
+          scope.schedule = response.data;
+          scope.schedule.forEach(function(scheduleDay) {
+            if (scheduleDay.lesson === scope.lesson) {
+              element.css('text-decoration', 'line-through');
+              return;
+            }
+          });
+        });
+      }
+    };
+  });
